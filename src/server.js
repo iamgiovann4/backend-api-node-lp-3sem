e7d7847126a277b87cc7a495d9dca60e47d34bbb
Giovanna <giovannasiqueiranicolau@gmail.com>
2023-02-10 17:19:17 -0300

    response json

--- src/server.js
+++ src/server.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const listaCursos = require('./bd/cursos.json')
+// const res = require('express/lib/response')
 const app = express()
 const port = 3200
 
 app.get('/', (req, res) => {
-  res.send('Hello Xoxo!')
+  const msg = [{nome: 'LP2'}, {nome: 'PJ3'}]
+  res.json(msg)
+})
+
+app.get('/curso', (req, res) => {
+  res.json(listaCursos)
+})
+
+app.post('/curso', (req, res) => {
+  res.json({message: "Olá POST curso JSON!"})
+})
+
+app.put('/curso', (req, res) => {
+  res.send('Fiz um update no curso!')
+})
+
+app.all('*', (req, res) => {
+  res.send('Erro')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
