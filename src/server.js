8ecee1bfdffb2df04fb499ed16f4705af803186e
Giovanna <giovannasiqueiranicolau@gmail.com>
2023-02-17 14:55:10 -0300

    banco de dados exportado

--- src/server.js
+++ src/server.js
@@ -1,80 +1,48 @@
 const express = require('express')
-const listaCursos = require('./bd/cursos.json')
-// const res = require('express/lib/response')
+const listaCursos = require('./db/cursos.json')
+const mysql = require('mysql')
+
+
 const app = express()
-const port = 3200
+const port = 3100
 
 app.get('/', (req, res) => {
-  const msg = [{nome: 'LP2'}, {nome: 'PJ3'}]
+  const msg = [{nome: 'LP2'},{nome: 'PJ3'}]
   res.json(msg)
 })
 
-app.get('/curso', (req, res) => {
-  res.json(listaCursos)
+app.get('/cursos', (req, res) => {
+  const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "apinode"
+  }) //conexão
+
+  con.connect((err) => {
+    if (err) throw err;
+    const sql = "select * from cursos;"
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      const objResult = JSON.parse(JSON.stringify(result))
+      res.json(objResult)
+    })
+  });
 })
 
-app.post('/curso', (req, res) => {
-  res.json({message: "Olá POST curso JSON!"})
+app.post('/cursos', (req, res) => {
+  res.json({message: 'POST Cursos JSON!'})
 })
 
-app.put('/curso', (req, res) => {
-  res.send('Fiz um update no curso!')
+app.put('/cursos', (req, res) => {
+  res.send('Fiz um update no Curso!')
 })
 
-app.all('*', (req, res) => {
-  res.send('Erro')
+app.all('*', (req, res) =>{
+  res.send('404 Rota não encontrada!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-// const express = require('express')
-// const listaCursos = require('./db/cursos.json')
-// const mysql = require('mysql')
-
-
-// const app = express()
-// const port = 3100
-
-// app.get('/', (req, res) => {
-//   const msg = [{nome: 'LP2'},{nome: 'PJ3'}]
-//   res.json(msg)
-// })
-
-// app.get('/cursos', (req, res) => {
-//   const con = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "apinode"
-//   })
-
-//   con.connect((err) => {
-//     if (err) throw err;
-//     const sql = "select * from cursos;"
-//     con.query(sql, function (err, result) {
-//       if (err) throw err;
-//       //TODO return result with JSON
-//     })
-//   });
-
-//   res.json(listaCursos)
-// })
-
-// app.post('/cursos', (req, res) => {
-//   res.json({message: 'POST Cursos JSON!'})
-// })
-
-// app.put('/cursos', (req, res) => {
-//   res.send('Fiz um update no Curso!')
-// })
-
-// app.all('*', (req, res) =>{
-//   res.send('404 Rota não encontrada!')
-// })
-
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
