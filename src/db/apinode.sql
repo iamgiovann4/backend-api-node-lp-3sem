252a2053d36a20f5df6c04e34bc642574efd825b
Giovanna <giovannasiqueiranicolau@gmail.com>
2023-03-17 15:24:05 -0300

    Add new db

--- src/db/apinode.sql
+++ src/db/apinode.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Fev-2023 às 22:02
+-- Tempo de geração: 17-Mar-2023 às 19:18
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.0.13
 
@@ -38,8 +38,44 @@ CREATE TABLE `cursos` (
 --
 
 INSERT INTO `cursos` (`id`, `nome`, `cargahoraria`) VALUES
-(1, 'LP2', '76 horas'),
-(2, 'PJ3', '76 horas');
+(4, 'HIS', '18 horas'),
+(6, 'MAT', '80 horas'),
+(7, 'PORT', '100 horas'),
+(9, 'QUI', '75 horas'),
+(10, 'LP1', '75 horas'),
+(11, 'PJ3', '1000 horas'),
+(12, 'FIS', '75 horas'),
+(13, 'IHC', '90 horas'),
+(14, 'JS', '100 horas'),
+(15, 'NODE', '75 horas');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(80) NOT NULL,
+  `age` int(2) NOT NULL,
+  `office` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nome`, `age`, `office`) VALUES
+(1, 'Giovanna', 19, 'Programadora'),
+(2, 'Vítor', 19, 'Vendedor'),
+(4, 'João', 22, 'Empresário'),
+(6, 'Joice', 25, 'Empresária'),
+(7, 'Moisés', 30, 'Gerente'),
+(8, 'Jefferson', 21, 'Vendedor'),
+(9, 'Isabela', 20, 'Limpeza'),
+(10, 'Josué', 26, 'Estágio'),
+(11, 'Jade', 24, 'Atendente');
 
 --
 -- Índices para tabelas despejadas
@@ -52,6 +88,12 @@ ALTER TABLE `cursos`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -59,7 +101,13 @@ ALTER TABLE `cursos`
 -- AUTO_INCREMENT de tabela `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
