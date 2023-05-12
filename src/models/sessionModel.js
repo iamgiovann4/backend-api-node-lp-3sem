a51aa29ceec10acd7e473f2206b0f1d29e56886a
iamgiovann4 <giovannasiqueiranicolau@gmail.com>
2023-05-12 07:37:14 -0300

    Corrigindo erro

--- src/models/sessionModel.js
+++ src/models/sessionModel.js
@@ -17,7 +17,7 @@ export const checkSession = (token, callback) => {
   const sql = `
     SELECT s.id_user, u.roles
     FROM sessions as s
-    INNER JOIN users as u
+    INNER JOIN usuarios as u
     ON s.id_user = u.id
     WHERE s.session = ?;
     `
@@ -32,9 +32,9 @@ export const checkSession = (token, callback) => {
   })
 }
 
-export const deleteSession = (email, token, callback) => {
-  const sql = 'DELETE FROM sessions WHERE id_user = (SELECT id FROM users WHERE email = ?) AND session = ?;'
-  const value = [email, token]
+export const deleteSession = (office, token, callback) => {
+  const sql = 'DELETE FROM sessions WHERE id_user = (SELECT id FROM usuarios WHERE office = ?) AND session = ?;'
+  const value = [office, token]
   con.query(sql, value, (err, result) => {
     if (err) {
       callback(err, null)
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,28 +6,31 @@ import sha256 from '../helper/sha256.js'
 
 const userSchema = z.object({
   id: 
-    z.number({message: "ID deve ser um valor numérico."}).optional(),
+    z.number({
+      required_error: "ID é obrigatório.",
+      invalid_type_error: "ID deve ser um número."
+    }),
   nome: 
     z.string({
       required_error: "Nome é obrigatório.",
       invalid_type_error: "Nome deve ser uma string."
     })
       .min(3, { message: "Nome deve ter no mínimo 3 caracteres." })
-      .max(50, { message: "Nome deve ter no máximo 100 caracteres." }),
+      .max(100, { message: "Nome deve ter no máximo 100 caracteres." }),
   age:
     z.string({ 
       required_error: "Idade é obrigatório.",
       invalid_type_error: "Idade deve ser um número"
     })
       .min(1, { message: "Idade deve ter no mínimo 1 caracteres" })
-      .max(2, { message: "Idade deve ter no máximo 2 caracteres." }),
+      .max(256, { message: "Idade deve ter no máximo 256 caracteres." }),
   office: 
     z.string({
       required_error: "Profissão é obrigatório.",
       invalid_type_error: "Profissão deve ser uma string."
     })
-      .min(6, { message: "Profissão deve ter no mínimo 6 caracteres"})
-      .max(256, { message: "Profissão deve ter no máximo 256 caracteres."})
+      .min(4, { message: "Profissão deve ter no mínimo 4 caracteres"})
+      .max(50, { message: "Profissão deve ter no máximo 50 caracteres."})
 })
 
 export const validateUser = (user) => {
@@ -36,12 +39,12 @@ export const validateUser = (user) => {
 }
 
 export const validateUserToUpdate = (user) => {
-  const partialUserSchema = userSchema.partial({ id: true });
+  const partialUserSchema = userSchema.partial({ age: true });
   return partialUserSchema.safeParse(user)
 }
 
 export const listAllUsers = (callback) => {
-  const sql = "SELECT id, nome, age, office, roles FROM usuarios;"
+  const sql = "SELECT id, nome, office, roles FROM usuarios;"
   con.query(sql, (err, result) => {
     if (err) {
       callback(err, null)
@@ -53,14 +56,12 @@ export const listAllUsers = (callback) => {
 }
 
 export const listId = (idUser, callback) => {
-  const sql = "SELECT id, nome, age, office FROM usuarios WHERE id = ?;"
+  const sql = "SELECT id, nome, office, roles FROM usuarios WHERE id = ?;"
   const values = [idUser]
   con.query(sql, values, (err, result) => {
     if (err) {
       callback(err, null) //a funcao callback é obg a passar 2 parametros
       console.log(`DB Error: ${err.sqlMessage}`)
-    } else if (result.length === 0) {
-      result.message = "Id não encontrado"
     } else {
       callback(null, result)
     }
@@ -100,9 +101,9 @@ export const deleteUser = (id, callback) => {
 }
 
 export const updateUser = (user, callback) => {
-  const { id, nome, age, office } = user
-  const sql = 'UPDATE usuarios SET nome = ?, age = ?, office = ?  WHERE id = ?;'
-  const values = [nome, age, office, id]
+  const { id, nome, office } = user
+  const sql = 'UPDATE usuarios SET nome = ?, office = ?  WHERE id = ?;'
+  const values = [nome, office, id]
 
   con.query(sql, values, (err, result) => {
     if (err) {
@@ -114,9 +115,9 @@ export const updateUser = (user, callback) => {
   })
 }
 
-export const loginUser = (email, pass, callback) => {
-  const sql = 'SELECT * FROM users WHERE email = ? AND pass = ?;'
-  const value = [email, sha256(pass)]
+export const loginUser = (office, age, callback) => {
+  const sql = 'SELECT * FROM usuarios WHERE office = ? AND age = ?;'
+  const value = [office, sha256(age)]
   con.query(sql, value, (err, result) => {
     if (err) {
       callback(err, null)
